752e48a419cc354215fa3ecdbcd596babe6b60f6
Anthony1x <antonyturtles@gmail.com>
2024-05-09 18:16:04 +0200

    Add LookAt and Normalize Helper functions

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -1,1 +1,14 @@
 #include "Helper.h"
+#include <cmath>
+
+Vector2 Helper::LookAt(Vector2 from, Vector2 at)
+{
+    return Helper::Normalize({at.x - from.x, at.y - from.y});
+}
+
+Vector2 Helper::Normalize(Vector2 vector)
+{
+    float length = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+
+    return {vector.x / length, vector.y / length};
+}
--- src/Helper.h
+++ src/Helper.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <raylib.h>
 
-// Static class containing helper function that are useful in all contexts-
+// Static class containing helper function that are useful in all contexts.
 class Helper
 {
   public:
     static const int screenWidth = 600;
     static const int screenHeight = 850;
+    static Vector2 LookAt(Vector2 from, Vector2 at);
+    static Vector2 Normalize(Vector2 vector);
 
     // This is a static class. Do not instantiate.
     Helper() = delete;
